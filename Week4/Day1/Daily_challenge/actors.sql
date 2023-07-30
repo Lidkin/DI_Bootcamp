776fbb041090a050a11e6506c111e75981978a77
Lidkin <lidkin79@gmail.com>
2023-07-30 15:39:04 +0300

    Daily_challenge added

---
+++ Week4/Day1/Daily_challenge/actors.sql
@@ -0,0 +1,27 @@
+-- create table actors(
+--   actor_id serial primary key,
+--   first_name varchar (50) not null,
+--   last_name varchar (100) not null,
+--   age date not null,
+--   number_oscars smallint not null
+-- )
+
+-- insert into actors(first_name, last_name, age, number_oscars)
+-- values
+--   ('Matt','Damon','1970-08-10',5),
+--   ('George','Clooney','1961-06-05',2),
+--   ('Tilda','Swinton','1960-11-05',1),
+--   ('Michelle','Yeoh','1962-08-06',1),
+--   ('Emma','Watson','1990-04-15',1),
+--   ('Chadwick','Boseman','1976-11-29',0),
+--   ('Gary','Oldman','1958-03-21',3)
+  
+--   select count(actor_id) from actors
+
+insert into actors(first_name, last_name, age, number_oscars)
+values ( , 'Stark', , 5)
+  
+  
+  
+  
+  
