4825ffc454186d38754960cdd168e34f8bc4b415
Lidkin <lidkin79@gmail.com>
2023-08-26 20:16:49 +0300

    w6d4 gold modified

--- Week6/Day4/Daily_challenge/gold.js
+++ Week6/Day4/Daily_challenge/gold.js
@@ -23,13 +23,3 @@ while (true) {
 }
 
 console.log(numbers)
-
-
-// Bonus: To do this Bonus look up how to work with nested for loops
-// Sort the numbers array in descending order, do so using for loops(Not using built -in sort methods).
-// The output should be: [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
-// Hint: The algorithm is called “Bubble Sort”
-// Use a temporary variable to swap values in the array.
-    // Use 2 “nested” for loops(Nested means one inside the other).
-// Add comments and console.log the result for each step, this will help you understand.
-    // Requirement: Don’t copy paste solutions from Google
